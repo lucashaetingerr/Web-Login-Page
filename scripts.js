efc3132a3651c6e4b0800c263605ea6ca049a11e
lucashaetingerr <lucashrr100@gmail.com>
2025-03-27 23:14:48 -0300

    fixed dimensions, positions and styles

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-// Códigos para implementar aprendizados web dev
-
 const container=document.getElementById('container');
 const registerBtn=document.getElementById('register');
 const loginBtn=document.getElementById('login');
